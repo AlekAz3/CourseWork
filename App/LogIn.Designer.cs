e33e4b71dc589e3f42413e3a6eb804331bf44b5e
ndreemiy <74467869+ndreemiy@users.noreply.github.com>
2021-12-12 23:16:14 +0300

    Готова форма Регистрации и Входа

--- App/LogIn.Designer.cs
+++ App/LogIn.Designer.cs
@@ -35,13 +35,14 @@ namespace App
             this.Text_Password = new System.Windows.Forms.TextBox();
             this.Btn_Enter = new System.Windows.Forms.Button();
             this.Btn_Reg = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_Log
             // 
             this.label_Log.AutoSize = true;
             this.label_Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label_Log.Location = new System.Drawing.Point(101, 43);
+            this.label_Log.Location = new System.Drawing.Point(8, 40);
             this.label_Log.Name = "label_Log";
             this.label_Log.Size = new System.Drawing.Size(64, 24);
             this.label_Log.TabIndex = 0;
@@ -50,16 +51,16 @@ namespace App
             // Text_Login
             // 
             this.Text_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Text_Login.Location = new System.Drawing.Point(105, 70);
+            this.Text_Login.Location = new System.Drawing.Point(12, 67);
             this.Text_Login.Name = "Text_Login";
-            this.Text_Login.Size = new System.Drawing.Size(163, 29);
+            this.Text_Login.Size = new System.Drawing.Size(250, 29);
             this.Text_Login.TabIndex = 1;
             // 
             // label_Pass
             // 
             this.label_Pass.AutoSize = true;
             this.label_Pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label_Pass.Location = new System.Drawing.Point(101, 122);
+            this.label_Pass.Location = new System.Drawing.Point(8, 99);
             this.label_Pass.Name = "label_Pass";
             this.label_Pass.Size = new System.Drawing.Size(76, 24);
             this.label_Pass.TabIndex = 2;
@@ -68,17 +69,17 @@ namespace App
             // Text_Password
             // 
             this.Text_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Text_Password.Location = new System.Drawing.Point(105, 149);
+            this.Text_Password.Location = new System.Drawing.Point(12, 126);
             this.Text_Password.Name = "Text_Password";
-            this.Text_Password.Size = new System.Drawing.Size(163, 29);
+            this.Text_Password.Size = new System.Drawing.Size(250, 29);
             this.Text_Password.TabIndex = 3;
             // 
             // Btn_Enter
             // 
             this.Btn_Enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Btn_Enter.Location = new System.Drawing.Point(105, 195);
+            this.Btn_Enter.Location = new System.Drawing.Point(12, 161);
             this.Btn_Enter.Name = "Btn_Enter";
-            this.Btn_Enter.Size = new System.Drawing.Size(163, 34);
+            this.Btn_Enter.Size = new System.Drawing.Size(250, 34);
             this.Btn_Enter.TabIndex = 4;
             this.Btn_Enter.Text = "Войти";
             this.Btn_Enter.UseVisualStyleBackColor = true;
@@ -86,19 +87,30 @@ namespace App
             // Btn_Reg
             // 
             this.Btn_Reg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Btn_Reg.Location = new System.Drawing.Point(105, 378);
+            this.Btn_Reg.Location = new System.Drawing.Point(12, 201);
             this.Btn_Reg.Name = "Btn_Reg";
-            this.Btn_Reg.Size = new System.Drawing.Size(163, 33);
+            this.Btn_Reg.Size = new System.Drawing.Size(250, 33);
             this.Btn_Reg.TabIndex = 5;
             this.Btn_Reg.Text = "Регистрация";
             this.Btn_Reg.UseVisualStyleBackColor = true;
             this.Btn_Reg.Click += new System.EventHandler(this.Btn_Reg_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Вход";
+            // 
             // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.ClientSize = new System.Drawing.Size(274, 244);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Btn_Reg);
             this.Controls.Add(this.Btn_Enter);
             this.Controls.Add(this.Text_Password);
@@ -120,6 +132,7 @@ namespace App
         private System.Windows.Forms.TextBox Text_Password;
         private System.Windows.Forms.Button Btn_Enter;
         private System.Windows.Forms.Button Btn_Reg;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- App/Register.Designer.cs
+++ App/Register.Designer.cs
@@ -35,16 +35,20 @@ namespace App
             this.Text_Surname = new System.Windows.Forms.TextBox();
             this.Text_Patronymic = new System.Windows.Forms.TextBox();
             this.Text_IndividualNumber = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Label_Ind = new System.Windows.Forms.Label();
+            this.Label_Passport = new System.Windows.Forms.Label();
+            this.Text_Passport = new System.Windows.Forms.TextBox();
+            this.Btn_Register = new System.Windows.Forms.Button();
+            this.Check_Adm = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(118, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 24);
+            this.label1.Size = new System.Drawing.Size(174, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Регистрация";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -54,7 +58,7 @@ namespace App
             this.Text_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Text_Name.Location = new System.Drawing.Point(12, 78);
             this.Text_Name.Name = "Text_Name";
-            this.Text_Name.Size = new System.Drawing.Size(221, 29);
+            this.Text_Name.Size = new System.Drawing.Size(232, 29);
             this.Text_Name.TabIndex = 1;
             // 
             // label2
@@ -63,16 +67,16 @@ namespace App
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(12, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.Size = new System.Drawing.Size(54, 24);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Введите ФИО";
+            this.label2.Text = "ФИО";
             // 
             // Text_Surname
             // 
             this.Text_Surname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Text_Surname.Location = new System.Drawing.Point(12, 113);
             this.Text_Surname.Name = "Text_Surname";
-            this.Text_Surname.Size = new System.Drawing.Size(221, 29);
+            this.Text_Surname.Size = new System.Drawing.Size(232, 29);
             this.Text_Surname.TabIndex = 3;
             // 
             // Text_Patronymic
@@ -80,33 +84,77 @@ namespace App
             this.Text_Patronymic.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Text_Patronymic.Location = new System.Drawing.Point(12, 148);
             this.Text_Patronymic.Name = "Text_Patronymic";
-            this.Text_Patronymic.Size = new System.Drawing.Size(221, 29);
+            this.Text_Patronymic.Size = new System.Drawing.Size(232, 29);
             this.Text_Patronymic.TabIndex = 4;
             // 
             // Text_IndividualNumber
             // 
-            this.Text_IndividualNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Text_IndividualNumber.Location = new System.Drawing.Point(12, 233);
+            this.Text_IndividualNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text_IndividualNumber.Location = new System.Drawing.Point(13, 209);
             this.Text_IndividualNumber.Name = "Text_IndividualNumber";
-            this.Text_IndividualNumber.Size = new System.Drawing.Size(217, 30);
+            this.Text_IndividualNumber.Size = new System.Drawing.Size(231, 29);
             this.Text_IndividualNumber.TabIndex = 5;
             // 
-            // label3
+            // Label_Ind
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(11, 204);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(356, 26);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Введите Индивидуальный Номер";
+            this.Label_Ind.AutoSize = true;
+            this.Label_Ind.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Ind.Location = new System.Drawing.Point(8, 180);
+            this.Label_Ind.Name = "Label_Ind";
+            this.Label_Ind.Size = new System.Drawing.Size(230, 24);
+            this.Label_Ind.TabIndex = 6;
+            this.Label_Ind.Text = "Индивидуальный Номер";
+            // 
+            // Label_Passport
+            // 
+            this.Label_Passport.AutoSize = true;
+            this.Label_Passport.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Passport.Location = new System.Drawing.Point(9, 241);
+            this.Label_Passport.Name = "Label_Passport";
+            this.Label_Passport.Size = new System.Drawing.Size(235, 24);
+            this.Label_Passport.TabIndex = 7;
+            this.Label_Passport.Text = "Серия и Номер паспорта";
+            // 
+            // Text_Passport
+            // 
+            this.Text_Passport.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Text_Passport.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text_Passport.Location = new System.Drawing.Point(13, 268);
+            this.Text_Passport.Name = "Text_Passport";
+            this.Text_Passport.Size = new System.Drawing.Size(232, 29);
+            this.Text_Passport.TabIndex = 8;
+            // 
+            // Btn_Register
+            // 
+            this.Btn_Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Btn_Register.Location = new System.Drawing.Point(12, 338);
+            this.Btn_Register.Name = "Btn_Register";
+            this.Btn_Register.Size = new System.Drawing.Size(233, 37);
+            this.Btn_Register.TabIndex = 9;
+            this.Btn_Register.Text = "Зарегистрироваться";
+            this.Btn_Register.UseVisualStyleBackColor = true;
+            // 
+            // Check_Adm
+            // 
+            this.Check_Adm.AutoSize = true;
+            this.Check_Adm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Check_Adm.Location = new System.Drawing.Point(12, 303);
+            this.Check_Adm.Name = "Check_Adm";
+            this.Check_Adm.Size = new System.Drawing.Size(153, 28);
+            this.Check_Adm.TabIndex = 11;
+            this.Check_Adm.Text = "Управляющий";
+            this.Check_Adm.UseVisualStyleBackColor = true;
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 461);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(253, 381);
+            this.Controls.Add(this.Check_Adm);
+            this.Controls.Add(this.Btn_Register);
+            this.Controls.Add(this.Text_Passport);
+            this.Controls.Add(this.Label_Passport);
+            this.Controls.Add(this.Label_Ind);
             this.Controls.Add(this.Text_IndividualNumber);
             this.Controls.Add(this.Text_Patronymic);
             this.Controls.Add(this.Text_Surname);
@@ -128,6 +176,10 @@ namespace App
         private System.Windows.Forms.TextBox Text_Surname;
         private System.Windows.Forms.TextBox Text_Patronymic;
         private System.Windows.Forms.TextBox Text_IndividualNumber;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label_Ind;
+        private System.Windows.Forms.Label Label_Passport;
+        private System.Windows.Forms.TextBox Text_Passport;
+        private System.Windows.Forms.Button Btn_Register;
+        private System.Windows.Forms.CheckBox Check_Adm;
     }
 }
