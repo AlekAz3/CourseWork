29818fc36e490b2e818172ea8ab4f4ec37eb2689
ndreemiy <74467869+ndreemiy@users.noreply.github.com>
2021-12-18 01:12:19 +0300

    Начало описания класса Employer

--- App/Admin.Designer.cs
+++ App/Admin.Designer.cs
@@ -37,11 +37,11 @@ namespace App
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(8, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.Size = new System.Drawing.Size(170, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "ФЫВАПРОЛ";
+            this.label1.Text = "Выберете Работника";
             // 
             // ChooseEmployer
             // 
@@ -49,7 +49,7 @@ namespace App
             this.ChooseEmployer.FormattingEnabled = true;
             this.ChooseEmployer.Location = new System.Drawing.Point(12, 40);
             this.ChooseEmployer.Name = "ChooseEmployer";
-            this.ChooseEmployer.Size = new System.Drawing.Size(150, 28);
+            this.ChooseEmployer.Size = new System.Drawing.Size(166, 28);
             this.ChooseEmployer.TabIndex = 2;
             // 
             // Admin
---
+++ App/Employer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Employer
+    {
+        private string name { get; set; }
+        private string surname { get; set; }
+        private string patronym { get; set; }
+        private string reprinands { get; set; }
+        private string rateperhour { get; set; }
+        private string penalty { get; set; }
+        private string sickleave { get; set; }
+        private string day { get; set; }
+
+
+        public Employer(string[] Employe)
+        {
+            name = Employe[0];
+            surname = Employe[1];
+            patronym = Employe[2];
+            reprinands = Employe[3];
+            rateperhour = Employe[4];
+            penalty = Employe[5];
+            sickleave = Employe[6];
+            day = Employe[7];
+        }
+
+    }
+}
